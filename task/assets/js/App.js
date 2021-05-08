b810d61d672f2fa776ce13956479603bf0b05ed0
MammadliHuseyn <Huseynym@code.edu.az>
2021-05-08 19:54:37 +0400

    Search added to table

--- task/assets/js/App.js
+++ task/assets/js/App.js
@@ -18,9 +18,9 @@ let setDataToTable = (data) => {
    data.forEach((user, idx) => {
       let loanStatus = isActiveLoan(user.loans);
       let loanApplyStatus = canApplyLoan(user.loans, user.salary);
-      const tbody = document.getElementById("user-table");
+      const tbody = document.getElementById("user-table-body");
       const row = `<tr>
-                     <th scope="row">${idx + 1}</th> 
+                     <td scope="row">${idx + 1}</td> 
                      <td><img src="${user.img}" class="w-25"/></td>
                      <td>${user.name} ${user.surname}</td>
                      <td>${user.salary.value} ${user.salary.currency}</td>
@@ -125,5 +125,26 @@ let setDataToModal = (user) => {
       tbody.innerHTML += row;
    })
 }
+
+let searchForName = () => {
+   
+   let input = document.getElementById("search-input");
+   let filter = input.value.toUpperCase();
+   let table = document.getElementById("user-table");
+   let tr = table.getElementsByTagName("tr");
+   for (i = 0; i < tr.length; i++) {
+      let td = tr[i].getElementsByTagName("td")[2];
+      if (td) {
+         let txtValue = td.textContent || td.innerText;
+         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+         } else {
+            tr[i].style.display = "none";
+         }
+      }
+   }
+}
+
+document.getElementById("search-input").addEventListener('keyup',searchForName);
 
 main();
